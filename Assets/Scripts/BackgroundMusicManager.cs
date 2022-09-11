8b2f66a932a35bfe84088cac0a383a1589d5c29d
Vanessa Krohn <vanessa.krohn@tum.de>
2022-09-11 18:31:15 +1000

    Audio clips added and background music implemented

---
+++ Assets/Scripts/BackgroundMusicManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class BackgroundMusicManager : MonoBehaviour
+{
+    private AudioSource _intro;
+    private AudioSource _backgroundNormal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var audios = GetComponents<AudioSource>();
+        _intro = audios[0];
+        _backgroundNormal = audios[1];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_intro.isPlaying && !_backgroundNormal.isPlaying)
+        {
+            _backgroundNormal.Play();
+        }
+    }
+}
