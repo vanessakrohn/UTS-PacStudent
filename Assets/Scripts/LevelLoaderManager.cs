b412c4dd985aa6eacdc41646ce0544066181d865
Vanessa Krohn <vanessa.krohn@tum.de>
2022-10-15 01:14:32 +1100

    HUD added

--- Assets/Scripts/LevelLoaderManager.cs
+++ Assets/Scripts/LevelLoaderManager.cs
@@ -1,24 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelLoaderManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
     }
 
     public void LoadLevel1()
     {
         SceneManager.LoadScene(1);
     }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.buildIndex == 1)
+        {
+            Button button = GameObject.FindWithTag("ExitButton").GetComponent<Button>();
+            button.onClick.AddListener(LoadStartScene);
+        }
+        
+    }
+
 }
