4fd7a7b717df5f05d05a187fe1a31c219b4a96e2
Vanessa Krohn <vanessa.krohn@tum.de>
2022-10-15 17:15:03 +1100

    PacStudent WASD movement added

--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PacStudentController : MonoBehaviour
@@ -8,48 +6,94 @@ public class PacStudentController : MonoBehaviour
     private Animator _animator;
     private static float Speed = 0.5f;
 
+    private enum UserInput
+    {
+        Up,
+        Left,
+        Down,
+        Right
+    }
+
+    private UserInput _lastInput = UserInput.Right;
+
     // Start is called before the first frame update
     void Start()
     {
         _tweener = gameObject.GetComponent<Tweener>();
         _animator = gameObject.GetComponent<Animator>();
         transform.position = new Vector3(Grid.TileSize, -Grid.TileSize, 0);
-        MoveRight();
     }
 
-    void Update() 
+    void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            _lastInput = UserInput.Up;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            _lastInput = UserInput.Left;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            _lastInput = UserInput.Down;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            _lastInput = UserInput.Right;
+        }
+
+        if (!_tweener.TweenExists(transform))
+        {
+            switch (_lastInput)
+            {
+                case UserInput.Up:
+                    MoveUp();
+                    break;
+                case UserInput.Left:
+                    MoveLeft();
+                    break;
+                case UserInput.Down:
+                    MoveDown();
+                    break;
+                case UserInput.Right:
+                    MoveRight();
+                    break;
+            }
+        }
     }
 
     private void MoveRight()
     {
         ResetTriggers();
-        _tweener.AddTween(transform, transform.position, transform.position + new Vector3(Grid.TileSize, 0,0), Speed);
+        _tweener.AddTween(transform, transform.position, transform.position + new Vector3(Grid.TileSize, 0, 0), Speed);
         _animator.SetTrigger("right");
     }
 
     private void MoveLeft()
     {
         ResetTriggers();
-        _tweener.AddTween(transform, transform.position, transform.position - new Vector3(Grid.TileSize, 0,0), Speed);
+        _tweener.AddTween(transform, transform.position, transform.position - new Vector3(Grid.TileSize, 0, 0), Speed);
         _animator.SetTrigger("left");
     }
 
     private void MoveDown()
     {
         ResetTriggers();
-        _tweener.AddTween(transform, transform.position, transform.position - new Vector3(0, Grid.TileSize,0), Speed);
+        _tweener.AddTween(transform, transform.position, transform.position - new Vector3(0, Grid.TileSize, 0), Speed);
         _animator.SetTrigger("down");
     }
 
     private void MoveUp()
     {
         ResetTriggers();
-        _tweener.AddTween(transform, transform.position, transform.position + new Vector3(0, Grid.TileSize,0), Speed);
+        _tweener.AddTween(transform, transform.position, transform.position + new Vector3(0, Grid.TileSize, 0), Speed);
         _animator.SetTrigger("up");
     }
-    
+
     private void ResetTriggers()
     {
         _animator.ResetTrigger("right");
